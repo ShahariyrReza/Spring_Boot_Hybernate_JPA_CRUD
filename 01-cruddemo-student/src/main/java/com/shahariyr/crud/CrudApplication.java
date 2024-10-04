9a007f0284b71310f8bc3ee1a621ddb26f674663
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-04 19:23:06 +0600

    add functionality for find by id method

--- 01-cruddemo-student/src/main/java/com/shahariyr/crud/CrudApplication.java
+++ 01-cruddemo-student/src/main/java/com/shahariyr/crud/CrudApplication.java
@@ -19,42 +19,67 @@ public class CrudApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentDao studentDao) {
 		return CommandLineRunner -> {
-			createStudent(studentDao);
+			//createStudent(studentDao);
+			
+			findStudent(studentDao);
 		};
 	}
 	
 
-	private void createStudent(StudentDao studentDao) {
-
-		// Create student
+	private void findStudent(StudentDao studentDao) {
+		// create a student;
 		System.out.println("Creating new student......");
-		Student tempStudent1 = new Student("Shahariyr", "Reza", "Shahariyr@hotmail.com");
-		Student tempStudent2 = new Student("Mazher", "Reza", "Mazher@hotmail.com");
-		Student tempStudent3 = new Student("Akash", "Rahman", "Akash.rahman@hotmail.com");
+		Student temStudent = new Student("Shibber", "Ahammed", "shibber.munna@gmail.com");
 		
+		// save the student;
+		System.out.println("Saving New student done");
+		studentDao.save(temStudent);
+		
+		// display the id of the student;
+		int theId = temStudent.getId();
+		System.out.println("Id of the student is :" + theId);
+		
+		// read the student based on id;
+		System.out.println("the student ID is :" + theId);
 
-		// Save student
-		System.out.println("Saveing the student.....");
-		studentDao.save(tempStudent1);
-		studentDao.save(tempStudent2);
-		studentDao.save(tempStudent3);
-
-		// display student
-		System.out.println("id" + " " + tempStudent1.getId());
-		System.out.println("First name" + " " + tempStudent1.getFirstName());
-		System.out.println("Last name" + " " + tempStudent1.getLastName());
-		System.out.println("Email" + " " + tempStudent1.getEmail());
+		// display student;
+		Student myStudent = studentDao.findById(theId);
+		System.out.println(myStudent);
 		
-		System.out.println("id" + " " + tempStudent2.getId());
-		System.out.println("First name" + " " + tempStudent2.getFirstName());
-		System.out.println("Last name" + " " + tempStudent2.getLastName());
-		System.out.println("Email" + " " + tempStudent2.getEmail());
-		
-		System.out.println("id" + " " + tempStudent3.getId());
-		System.out.println("First name" + " " + tempStudent3.getFirstName());
-		System.out.println("Last name" + " " + tempStudent3.getLastName());
-		System.out.println("Email" + " " + tempStudent3.getEmail());
 	}
+
+
+//	private void createStudent(StudentDao studentDao) {
+//
+//		// Create student
+//		System.out.println("Creating new student......");
+//		Student tempStudent1 = new Student("Shahariyr", "Reza", "Shahariyr@hotmail.com");
+//		Student tempStudent2 = new Student("Mazher", "Reza", "Mazher@hotmail.com");
+//		Student tempStudent3 = new Student("Akash", "Rahman", "Akash.rahman@hotmail.com");
+//		
+//
+//		// Save student
+//		System.out.println("Saving the student.....");
+//		studentDao.save(tempStudent1);
+//		studentDao.save(tempStudent2);
+//		studentDao.save(tempStudent3);
+//
+//		// display student
+//		System.out.println("id" + " " + tempStudent1.getId());
+//		System.out.println("First name" + " " + tempStudent1.getFirstName());
+//		System.out.println("Last name" + " " + tempStudent1.getLastName());
+//		System.out.println("Email" + " " + tempStudent1.getEmail());
+//		
+//		System.out.println("id" + " " + tempStudent2.getId());
+//		System.out.println("First name" + " " + tempStudent2.getFirstName());
+//		System.out.println("Last name" + " " + tempStudent2.getLastName());
+//		System.out.println("Email" + " " + tempStudent2.getEmail());
+//		
+//		System.out.println("id" + " " + tempStudent3.getId());
+//		System.out.println("First name" + " " + tempStudent3.getFirstName());
+//		System.out.println("Last name" + " " + tempStudent3.getLastName());
+//		System.out.println("Email" + " " + tempStudent3.getEmail());
+//	}
 }
 
 

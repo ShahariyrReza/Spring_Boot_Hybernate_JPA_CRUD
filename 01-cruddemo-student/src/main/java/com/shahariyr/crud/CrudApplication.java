ed118f00ae1452ed3e2f0aee02547358d43eeac8
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-04 23:46:42 +0600

    find all student data

--- 01-cruddemo-student/src/main/java/com/shahariyr/crud/CrudApplication.java
+++ 01-cruddemo-student/src/main/java/com/shahariyr/crud/CrudApplication.java
@@ -1,5 +1,7 @@
 package com.shahariyr.crud;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,75 +16,81 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
-	
 
 	@Bean
 	CommandLineRunner commandLineRunner(StudentDao studentDao) {
 		return CommandLineRunner -> {
-			//createStudent(studentDao);
-			
-			findStudent(studentDao);
+			// createStudent(studentDao);
+
+			// findStudent(studentDao);
+
+			findAllStudent(studentDao);
 		};
 	}
-	
+
+	private void findAllStudent(StudentDao studentDao) {
+
+		// get a list of student
+		List<Student> theStudents = studentDao.findall();
+
+		// display the list of student.
+
+		for (Student temStudent : theStudents) {
+			System.out.println(temStudent);
+		}
+
+	}
 
 	private void findStudent(StudentDao studentDao) {
 		// create a student;
 		System.out.println("Creating new student......");
-		Student temStudent = new Student("Shibber", "Ahammed", "shibber.munna@gmail.com");
-		
+		Student temStudent = new Student("Rakib", "Ahammed", "shibber.munna@gmail.com");
+
 		// save the student;
 		System.out.println("Saving New student done");
 		studentDao.save(temStudent);
-		
+
 		// display the id of the student;
 		int theId = temStudent.getId();
 		System.out.println("Id of the student is :" + theId);
-		
+
 		// read the student based on id;
 		System.out.println("the student ID is :" + theId);
 
 		// display student;
 		Student myStudent = studentDao.findById(theId);
 		System.out.println(myStudent);
-		
+
 	}
 
+	private void createStudent(StudentDao studentDao) {
 
-//	private void createStudent(StudentDao studentDao) {
-//
-//		// Create student
-//		System.out.println("Creating new student......");
-//		Student tempStudent1 = new Student("Shahariyr", "Reza", "Shahariyr@hotmail.com");
-//		Student tempStudent2 = new Student("Mazher", "Reza", "Mazher@hotmail.com");
-//		Student tempStudent3 = new Student("Akash", "Rahman", "Akash.rahman@hotmail.com");
-//		
-//
-//		// Save student
-//		System.out.println("Saving the student.....");
-//		studentDao.save(tempStudent1);
-//		studentDao.save(tempStudent2);
-//		studentDao.save(tempStudent3);
-//
-//		// display student
-//		System.out.println("id" + " " + tempStudent1.getId());
-//		System.out.println("First name" + " " + tempStudent1.getFirstName());
-//		System.out.println("Last name" + " " + tempStudent1.getLastName());
-//		System.out.println("Email" + " " + tempStudent1.getEmail());
-//		
-//		System.out.println("id" + " " + tempStudent2.getId());
-//		System.out.println("First name" + " " + tempStudent2.getFirstName());
-//		System.out.println("Last name" + " " + tempStudent2.getLastName());
-//		System.out.println("Email" + " " + tempStudent2.getEmail());
-//		
-//		System.out.println("id" + " " + tempStudent3.getId());
-//		System.out.println("First name" + " " + tempStudent3.getFirstName());
-//		System.out.println("Last name" + " " + tempStudent3.getLastName());
-//		System.out.println("Email" + " " + tempStudent3.getEmail());
-//	}
+		// Create student
+		System.out.println("Creating new student......");
+		Student tempStudent1 = new Student("Shahariyr", "Reza", "Shahariyr@hotmail.com");
+		Student tempStudent2 = new Student("Mazher", "Reza", "Mazher@hotmail.com");
+		Student tempStudent3 = new Student("Akash", "Rahman", "Akash.rahman@hotmail.com");
+
+		// Save student
+		System.out.println("Saving the student.....");
+		studentDao.save(tempStudent1);
+		studentDao.save(tempStudent2);
+		studentDao.save(tempStudent3);
+
+		// display student
+		System.out.println("id" + " " + tempStudent1.getId());
+		System.out.println("First name" + " " + tempStudent1.getFirstName());
+		System.out.println("Last name" + " " + tempStudent1.getLastName());
+		System.out.println("Email" + " " + tempStudent1.getEmail());
+
+		System.out.println("id" + " " + tempStudent2.getId());
+		System.out.println("First name" + " " + tempStudent2.getFirstName());
+		System.out.println("Last name" + " " + tempStudent2.getLastName());
+		System.out.println("Email" + " " + tempStudent2.getEmail());
+
+		System.out.println("id" + " " + tempStudent3.getId());
+		System.out.println("First name" + " " + tempStudent3.getFirstName());
+		System.out.println("Last name" + " " + tempStudent3.getLastName());
+		System.out.println("Email" + " " + tempStudent3.getEmail());
+	}
 }
-
-
-
-
-
